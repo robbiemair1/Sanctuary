9ef71a23366ece811492a8ed168cd0411285e119
robbiemair1 <robbie@robbiemair.com>
2026-06-03 20:21:30 +0100

    Remove dotenv import from Vercel API handler

--- api/send-contact.js
+++ api/send-contact.js
@@ -1,6 +1,5 @@
 const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
-dotenv.config();
+
 function escapeHtml(input) {
   return String(input || '')
     .replace(/&/g, '&amp;')
